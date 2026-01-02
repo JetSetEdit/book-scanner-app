630d04c76028c314e0c797f9e73ca0daadd1975a
JetSetEdit <admin@jetsetedit.au>
2026-01-02 23:56:44 +1100

    chore: bump version to 1.01.5

--- lib/config/version.ts
+++ lib/config/version.ts
@@ -5,7 +5,7 @@
  * Update this when deploying new versions.
  */
 
-export const APP_VERSION = "1.01.4"
+export const APP_VERSION = "1.01.5"
 export const APP_VERSION_LABEL = "Public Beta 1.01"
 export const APP_BUILD_DATE = "2026-01-02" // YYYY-MM-DD - Update on each deployment
 
