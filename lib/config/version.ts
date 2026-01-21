91a302c3139e48199a15b8f60a245527939d751f
JetSetEdit <admin@jetsetedit.au>
2026-01-21 18:43:23 +1100

    chore: changelog 1.03.51

--- lib/config/version.ts
+++ lib/config/version.ts
@@ -5,7 +5,7 @@
  * Update this when deploying new versions.
  */
 
-export const APP_VERSION = "1.03.51"
+export const APP_VERSION = "1.03.52"
 export const APP_VERSION_LABEL = "Public Beta"
 export const APP_BUILD_DATE = "2026-01-21" // YYYY-MM-DD - Update on each deployment
 
